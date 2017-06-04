3009dd58aafd1a11c457491b244382d619f10d82
rus323232 <extreme33@yandex.ru>
2017-06-04 21:58:35 +0400

    final commit

--- backend/class/InfoFileHandler.php
+++ backend/class/InfoFileHandler.php
@@ -17,7 +17,6 @@ class InfoFileHandler {
         try {
             $this->filePath = $filePath;
             $this->file = fopen($filePath, $key);
-
             if($this->file) {
                 if (filesize($filePath) > 0) {
                     $this->setParams();
@@ -27,7 +26,6 @@ class InfoFileHandler {
             } else {
                 throw new Exception('Error with open .txt file');
             }
-
         } catch (Exception $e) {
             echo $e->getMessage();
         }
@@ -41,18 +39,14 @@ class InfoFileHandler {
         $unitProperties = array();
         $f = $this->fileContent;
         $lines  = explode("\r\n",$f);
-
         foreach ($lines as $prop) {
             $result = explode("|", $prop);
-
             if (isset($result[0]) && isset($result[1])) {
                 $propertyName = strtolower($result[0]);
                 $propertyValue = $result[1];
                 $unitProperties[$propertyName] = $propertyValue;
             }
-
         }
-
         return $unitProperties;
     }
 
@@ -64,22 +58,18 @@ class InfoFileHandler {
 
     public function getUnitProp ($propName) {
         $prop = strtolower($propName);
-
         if ($prop == "all") {
             return $this->unitProps;
         }
-
         if (!array_key_exists($prop, $this->unitProps)) {
             return "Pass the property name";
         }
-
         return $this->unitProps[$prop];
     }
 
     public function readAsIs() {
         $f = $this->fileContent;
         $answer = str_replace("\r\n", "<br>", $f);
-
         return $answer;
     }
 
@@ -87,15 +77,11 @@ class InfoFileHandler {
         if ((gettype($args) != 'array')) {
             return false;
         }
-
         $schema = "";
-
         foreach ($args as $prop => $value) {
             $schema .= $prop. "|" .$value. "\r\n";
         }
-
         fwrite($this->file, $schema);
-
         return true;
     }
 
@@ -103,25 +89,20 @@ class InfoFileHandler {
         if ($this->file) {
             fclose($this->file);
         }
-
     }
 
     static function scanForUnits ($dir) {
         if (!is_dir($dir)) {
             return "Dir not exist";
         }
-
         $files = scandir($dir);
         $units = array();
-
         foreach ($files as $value) {
             if (is_dir($dir . "/" . $value) && $value != "." && $value != "..") {
                 array_push($units, $value);
             }
         }
-
         sort($units);
-
         return $units;
     }
 }
